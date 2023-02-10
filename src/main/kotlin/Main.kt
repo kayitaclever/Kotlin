ea0dc6fb1f2332fda89d3a0a634e507386cbacd6
kayitaclever <kayitaramirwaaclever@gmail.com>
2023-02-10 09:08:06 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    printName("Clever")
+
+    var x= 64 % 3
+    println(x)
+var y= 56+40+15+32
+    println(y)
+    println("Artistic Eye")
+}
+fun printName(name:String)  {
+   println("Hello $name")
+
+}
+fun modulus (num1: Int, num2: Int):Int{
+    var modulus= num1 % num2
+    return modulus
+
+}
+fun addition(num1: Int, num2:Int,num3:Int,num4:Int): Int{
+    var addition=num1+num2+num3+num4
+    return addition
+}
+fun fact (me:String){
+    println("me")
+}
